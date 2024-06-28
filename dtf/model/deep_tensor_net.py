aa3f0fd0bd1d58112ac5e233186150b29db2a3b5
Manos Theodosis <manostheodosis@gmail.com>
2024-06-28 15:24:10 -0400

    moved init from DTN to DTN_conv to control hypercubes

--- dtf/model/deep_tensor_net.py
+++ dtf/model/deep_tensor_net.py
@@ -135,12 +135,6 @@ class HyperCube(nn.Module):
         if layer_type == 'FC': # Hack for now
             tensor_size_list[0] = [N**2, N, N]
 
-        # Should have a trainable convolution filter here
-        # self.filter = nn.Parameter(init_scale * torch.randn(3, 3) / math.sqrt(2))
-        # (I actually think the filter is incorporate into W)
-        #
-        # Separate class: subclass of Deep_Tensor_Net(_w)
-
         self.einsum_str = einsum_str
         self.idx_appearance_dict = idx_appearance_dict
         self.input_str_list = input_str_list
@@ -152,7 +146,6 @@ class HyperCube(nn.Module):
         self.N = N
         W = self.net_Weight
         self.W_shape_cum = get_W_shape_cum(W)
-        # self.store_init_weights()
 
     def initialize_Tensors(self, tensor_size_list, init_scale0=1):
         tensor_depth = len(tensor_size_list)
@@ -200,62 +193,20 @@ class Deep_Tensor_Net(Base_Model):
     def __init__(self, *args, **kwargs):  # init_val = 1) -> None:
         super().__init__(*args, **kwargs)
         # self.initialize_weights(*args, **kwargs)
-        N, r = kwargs.get("N", None), kwargs.get("r", None)
-        init_scale = kwargs.get("init_scale", 1)
-        layer_type = kwargs.get("layer_type", None)
-        decomposition_type = kwargs.get("decomposition_type", 'FC')
+        # N, r = kwargs.get("N", None), kwargs.get("r", None)
+        # init_scale = kwargs.get("init_scale", 1)
+        # layer_type = kwargs.get("layer_type", None)
+        # decomposition_type = kwargs.get("decomposition_type", 'FC')
 
-        self.hypercubes = nn.ModuleList([HyperCube(N, r, init_scale, decomposition_type, layer_type)])
+        # self.hypercubes = nn.ModuleList([HyperCube(N, r, init_scale, decomposition_type, layer_type)])
 
-        W = self.net_Weight  # JUST TO INITIALIZE.
+        # W = self.net_Weight  # JUST TO INITIALIZE.
 
     def evaluate(self, data, *args, **kwargs):
         loss, acc, out, (other_outputs, x, out, label) = super().evaluate(
             data, *args, **kwargs
         )
         return loss, acc, out, other_outputs
-
-    # def initialize_weights(self, *args, **kwargs):
-    #     N, r, decomposition_type = kwargs.get("N", None), kwargs.get("r", None), "FC"
-    #     init_scale = kwargs.get("init_scale", 1)
-    #     r = r or (min(N) if isinstance(N, (tuple, list)) else N)
-    #     einsum_str, shared_idx0 = get_einsum_str(decomposition_type)
-    #     tensor_size_list, idx_appearance_dict, input_str_list, ext_indices, _ = (
-    #         get_tensor_size(N, r, einsum_str)
-    #     )
-
-    #     if self.layer_type == "FC":
-    #         tensor_size_list[0] = [N**2, N, N]
-
-    #     self.einsum_str = einsum_str
-    #     self.idx_appearance_dict = idx_appearance_dict
-    #     self.input_str_list = input_str_list
-    #     self.ext_indices = ext_indices
-    #     self.decomposition_type = decomposition_type
-
-    #     self.layers = self.initialize_Tensors(
-    #         tensor_size_list, init_scale
-    #     )  # , random_init)
-
-    #     self.N = N
-    #     W = self.net_Weight
-    #     self.W_shape_cum = get_W_shape_cum(W)
-    #     # self.store_init_weights()
-
-    # def initialize_Tensors(self, tensor_size_list, init_scale0=1):
-    #     tensor_depth = len(tensor_size_list)
-    #     init_scale = init_scale0 ** (1 / tensor_depth)
-
-    #     layers = []
-    #     for siz in tensor_size_list:
-    #         layer = Tensor_Layer(siz, init_scale=init_scale)
-    #         layers.append(layer)
-
-    #     return nn.ModuleList(layers)
-
-    # @property
-    # def T_param_list(self):
-    #     return [layer.tensor for layer in self.layers]
 
     def forward(
         self, x: Tensor, W=None, *args, **kwargs
@@ -268,7 +219,7 @@ class Deep_Tensor_Net(Base_Model):
         out = self.read_from_Tensor(W, x)
         return out, (W,)
 
-    def read_from_Tensor(self, W, x):  # Modify this to handle tuple of weight tensors
+    def read_from_Tensor(self, W, x):
         if x.dtype == torch.int64:  # x is tensor of indices
             out = self.index_select(W,x)
         else:
@@ -292,43 +243,45 @@ class Deep_Tensor_Net(Base_Model):
 
     @property
     def factor_list(self):
-        return  list(hypercube.factor_list for hypercube in self.hypercubes)
-        # return self.T_param_list
+        return list(hypercube.factor_list for hypercube in self.hypercubes)
 
     @property
     def T_list_no_grad(self):
         return list(hypercube.T_list_no_grad for hypercube in self.hypercubes)
-        # return [T.detach() for T in self.factor_list]
 
     @property
     def net_Weight(self): # returns a tuple of net_Weight tensors
-        # return (hypercube.net_Weight for hypercube in self.hypercubes)
         self._net_Weight = list(hypercube.net_Weight for hypercube in self.hypercubes)
         return self._net_Weight  # .permute(2,0,1)
 
     def get_svd(self):
         return list([hypercube.get_svd() for hypercube in self.hypercubes])
-        # return compute_svd_all(self.factor_list, self.idx_appearance_dict)
 
     def get_imbalance2(self):
         return list(hypercube.get_imbalance2() for hypercube in self.hypercubes)
 
     def manual_L2_loss(self):
         return sum(hypercube.manual_L2_loss() for hypercube in self.hypercubes)
-        # return compute_Manual_L2_all(self.factor_list)
 
     def Custom_L2_loss(self):  # mix_coeff=0.00):
         return sum(hypercube.Custom_L2_loss() for hypercube in self.hypercubes)
-        # loss = compute_Custom_L2_all(self.factor_list, self.idx_appearance_dict)
-        # return loss
 
 
 class Deep_Tensor_Net_conv(Deep_Tensor_Net):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # self.initialize_weights(*args, **kwargs)
+        N, r = kwargs.get("N", None), kwargs.get("r", None)
+        init_scale = kwargs.get("init_scale", 1)
+        layer_type = kwargs.get("layer_type", None)
+        decomposition_type = kwargs.get("decomposition_type", 'FC')
+        self.hypercubes = nn.ModuleList([HyperCube(N, r, init_scale, decomposition_type, layer_type)])
+
+        W = self.net_Weight  # JUST TO INITIALIZE.
+
         init_scale = kwargs.get("init_scale", 1)
         n_vectors = kwargs.get("n_vectors", 1)
+
         # convolution weights
         # self.conv_weight = nn.Parameter(init_scale * torch.randn(3, 3) / math.sqrt(2))
         if self.layer_type == "FC":
@@ -339,21 +292,13 @@ class Deep_Tensor_Net_conv(Deep_Tensor_Net):
             self.conv_weight = nn.Parameter(init_scale * torch.randn(6, n_vectors))
 
     def read_from_Tensor(self, W, x):
-        W = W[0] # to be fixed
         if x.dtype == torch.int64:  # x is tensor of indices
             out = self.index_select(W, x)
         else:
-            # (this split is weird, data should have 3 tensors, x,y,z)
-            # import pdb; pdb.set_trace()
-            # x1,x2 = (tensor.squeeze(dim=1) for tensor in x.split(1,dim=1))
-            # out = torch.einsum('ijk,bi,bj->bk',W,x1,x2)
-            # here I should performing the convolution with the filter?
-            # out = torch.einsum('ijk,bi,j->bk',W,x1,self.conv_weight)
-            # out = torch.einsum('ijk,bi,j->bk',W,x,self.conv_weight)
-            if isinstance(W, tuple):
+            if len(W) > 1:
                 raise ValueError("Deep_Tensor_Net: read_from_Tensor: W is tuple of tensors. This should be used with Deep_Tensor_Net_conv2d.")
             else:
-                # print(W.shape, x.shape, self.conv_weight.shape)
+                W = W[0]
                 out = torch.einsum("ijk,bi,jc->bck", W, x, self.conv_weight)
         return out
 
@@ -367,104 +312,30 @@ class Deep_Tensor_Net_conv(Deep_Tensor_Net):
         return self
 
 
-class Deep_Tensor_Net_conv2d(Base_Model):
+class Deep_Tensor_Net_conv2d(Deep_Tensor_Net):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.initialize_weights(*args, **kwargs)
+
         init_scale = kwargs.get("init_scale", 1)
         n_vectors = kwargs.get("n_vectors", 1)
         # convolution weights
         # self.conv_weight = nn.Parameter(init_scale * torch.randn(3, 3) / math.sqrt(2))
         if self.layer_type == "FC":
             self.conv_weight = nn.Parameter(
-                init_scale * torch.randn(6**2, n_vectors)
+                init_scale * torch.randn(6**2, 6**2) # eventually should be (6**2, 6**2, n_vectors) (think of n_vectors as batch)
             )  # should match data.py line 309
         else:
-            self.conv_weight = nn.Parameter(init_scale * torch.randn(6, n_vectors))
+            self.conv_weight = nn.Parameter(init_scale * torch.randn(6, 6)) # eventually should be (6, 6, n_vectors) (think of n_vectors as batch)
 
-    def initialize_weights(self, *args, **kwargs):
-        N, r, decomposition_type = kwargs.get("N", None), kwargs.get("r", None), "FC"
-        init_scale = kwargs.get("init_scale", 1)
-        r = r or (min(N) if isinstance(N, (tuple, list)) else N)
-        einsum_str, shared_idx0 = get_einsum_str(decomposition_type)
-        tensor_size_list, idx_appearance_dict, input_str_list, ext_indices, _ = (
-            get_tensor_size(N, r, einsum_str)
-        )
-
-        if self.layer_type == "FC":
-            tensor_size_list[0] = [N**2, N, N]
-
-        self.einsum_str = einsum_str
-        self.idx_appearance_dict = idx_appearance_dict
-        self.input_str_list = input_str_list
-        self.ext_indices = ext_indices
-        self.decomposition_type = decomposition_type
-
-        self.layers = self.initialize_Tensors(
-            tensor_size_list, init_scale
-        )  # , random_init)
-        self.N = N
-        W = self.net_Weight
-        self.W_shape_cum = get_W_shape_cum(W)
-        # self.store_init_weights()
-
-    def initialize_Tensors(self, tensor_size_list, init_scale0=1):
-        tensor_depth = len(tensor_size_list)
-        init_scale = init_scale0 ** (1 / tensor_depth)
-        import pdb
-
-        pdb.set_trace
-        layers = []
-        for siz in tensor_size_list:
-            layer = Tensor_Layer2d(siz, init_scale=init_scale)
-            layers.append(layer)
-
-        return nn.ModuleList(layers)
-
-    @property
-    def T_param_list_x(self):
-        return [layer.tensor_x for layer in self.layers]
-
-    @property
-    def T_param_list_y(self):
-        return [layer.tensor_y for layer in self.layers]
-
-    # @property
-    def net_Weight2d(self, update):  # you can remove update and return both
-        if update == "x":
-            self._net_Weight_x = tensor_prod(self.T_param_list_x, self.einsum_str)
-            return self._net_Weight_x  # .permute(2,0,1)
-        elif update == "y":
-            self._net_Weight_y = tensor_prod(self.T_param_list_y, self.einsum_str)
-            return self._net_Weight_y  # .permute(2,0,1)
-        else:
-            raise ValueError("update should be x or y")
-
-    def forward(
-        self, x: Tensor, W=None, *args, **kwargs
-    ):  # -> Tuple[Tensor, Union[Tensor, None], Union[Tensor, None]]:
-        if W is None:
-            if kwargs.get("train_or_test") == "train":
-                W_x = self.net_Weight2d("x")
-                W_y = self.net_Weight2d("y")
-            else:
-                W_x = self._net_Weight_x  # existing weight
-                W_y = self._net_Weight_y  # existing weight
-        out = self.read_from_Tensor2d(W_x, W_y, x)
-        return out, (W,)
-
-    def read_from_Tensor2d(self, W_x, W_y, x):
+    def read_from_Tensor(self, W, x):
         if x.dtype == torch.int64:  # x is tensor of indices
-            out = self.index_select(W_x, x)
+            out = self.index_select(W, x)
         else:
-            # (this split is weird, data should have 3 tensors, x,y,z)
-            # import pdb; pdb.set_trace()
-            # x1,x2 = (tensor.squeeze(dim=1) for tensor in x.split(1,dim=1))
-            # out = torch.einsum('ijk,bi,bj->bk',W,x1,x2)
-            # here I should performing the convolution with the filter?
-            # out = torch.einsum('ijk,bi,j->bk',W,x1,self.conv_weight)
-            # out = torch.einsum('ijk,bi,j->bk',W,x,self.conv_weight)
-            out = torch.einsum("iko,jlp,bij,kl->bop", W_x, W_y, x, self.conv_weight)
+            print(len(W))
+            if len(W) > 1:
+                out = torch.einsum("iko,jlp,bij,kl->bop", W[0], W[1], x, self.conv_weight)
+            else:
+                raise ValueError("Deep_Tensor_Net_conv2d: read_from_Tensor: W is NOT tuple of tensors. This should be used with Deep_Tensor_Net_conv.")
         return out
 
     def normalize(self):
